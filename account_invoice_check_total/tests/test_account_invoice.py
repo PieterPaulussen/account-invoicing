bcf81cc21219af497bd47a6ffefb68fb375e2c11
Pieter Paulussen <pieterpaulussen@code-source.be>
2020-02-19 14:14:51 +0100

    [FIX] Insert product when creating invoice line due to sale module restrictions

--- account_invoice_check_total/tests/test_account_invoice.py
+++ account_invoice_check_total/tests/test_account_invoice.py
@@ -16,6 +16,7 @@ class TestAccountInvoice(TransactionCase):
         self.account_invoice = self.env['account.invoice']
         self.account_model = self.env['account.account']
         self.account_invoice_line = self.env['account.invoice.line']
+        self.product = self.env['product.product'].search([], limit=1)
         self.current_user = self.env.user
         # Add current user to group: group_supplier_inv_check_total
         self.env.ref(GROUP_AICT).write({'users': [(4, self.current_user.id)]})
@@ -32,6 +33,8 @@ class TestAccountInvoice(TransactionCase):
         self.invoice_line = self.account_invoice_line.create(
             {'name': 'Test invoice line',
              'account_id': self.invoice_account.id,
+             'product_id': self.product.id,
+             'uom_id': self.product.uom_id.id,
              'quantity': 1.000,
              'price_unit': 2.99})
 
